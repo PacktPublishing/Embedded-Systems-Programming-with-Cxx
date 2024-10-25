71583705eaadda1b07f50ab00f46054762a56d27
Heitor Peralles <heitorgp@gmail.com>
2024-10-25 14:32:44 -0300

    Printing BB file in the script

--- Chapter01/Chapter01_Example04_Yocto_Customized_Layer/step08_copy_custom_app_recipe_file.sh
+++ Chapter01/Chapter01_Example04_Yocto_Customized_Layer/step08_copy_custom_app_recipe_file.sh
@@ -2,3 +2,4 @@
 
 mkdir -p poky/meta-example-layer/recipes-support/example-app
 cp example_app.bb poky/meta-example-layer/recipes-support/example-app/
+cat poky/meta-example-layer/recipes-support/example-app/example_app.bb
